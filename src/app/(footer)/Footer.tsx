1037ef59d60b0fcf146cfe05b7ccd29af2d62ac5
kamrul111 <si2271485@gmail.com>
2025-06-18 11:29:24 +0600

    added

---
+++ src/app/(footer)/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-red-700'>
+            <h1>this is the footer</h1>
+        </div>
+    );
+};
+
+export default Footer;
